b8a51ca447239fc55335c30a1d45cce3f4e3d7e1
gayak <gaya.kps@gmail.com>
2022-06-02 09:33:23 +0900

    Initial commit

---
+++ src/main/java/gaya/pe/kr/core/ItemMail.java
@@ -0,0 +1,17 @@
+package gaya.pe.kr.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemMail extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
